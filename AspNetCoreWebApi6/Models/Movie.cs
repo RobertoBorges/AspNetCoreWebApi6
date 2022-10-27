02b15fd7e1b594f97a5df5eeeea9ec5124973de9
Roberto Borges <roberto.borges@outlook.com>
2022-10-27 08:18:54 -0400

    removing warnings

--- AspNetCoreWebApi6/Models/Movie.cs
+++ AspNetCoreWebApi6/Models/Movie.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
-
-        public string Genre { get; set; }
+        public string Title { get; set; } = string.Empty;
+        
+        public string Genre { get; set; } = string.Empty;
 
         public DateTime ReleaseDate { get; set; }
 
--- AspNetCoreWebApi6/Models/Session.cs
+++ AspNetCoreWebApi6/Models/Session.cs
@@ -3,8 +3,8 @@
     public class Session
     {
         public int Id { get; set; }
-        public string Day { get; set; }
-        public string Room { get; set; }
+        public string Day { get; set; } = string.Empty;
+        public string Room { get; set; } = string.Empty;
         public List<Movie>? Movies { get; set; }
     }
 }
